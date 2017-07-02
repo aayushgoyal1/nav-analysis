c84fe08c15a1af6b80da7d6df3db6a2e0c075f42
Aayush Goyal <goyalaayush@yahoo.com>
2017-07-02 09:33:44 -0500

    Update init.py

--- test/init.py
+++ test/init.py
@@ -1,1 +1,12 @@
+#
+#
+# This is approximately pure overhead required to make a Python package.
+#
+# ..  py:module:: __init__
+#
+# ::
 
+"""navanalysis/test 
+Requires Python 3.x
+"""
+__version__ = "2.1"
